0c6364aba27bdce08e1e600718d46d8dba7406fa
Yasmine Masmoudi <87315006+yasminemasmoudi@users.noreply.github.com>
2022-11-26 22:48:51 +0100

    unit testcases updated

--- src/main/java/com/example/springboot/controller/SpringController.java
+++ src/main/java/com/example/springboot/controller/SpringController.java
@@ -1,7 +1,5 @@
 package com.example.springboot.controller;
 
-import java.time.LocalDateTime;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
